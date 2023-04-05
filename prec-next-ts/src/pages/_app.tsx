30b7d073422983787193e1a0f59b1ce62ea24888
cpk0709 <cpk0709@gmail.com>
2023-04-05 23:26:36 +0900

    feat : Head 컴포넌트 설정

--- prec-next-ts/src/pages/_app.tsx
+++ prec-next-ts/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/src/styles/globals.css";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../styles/reset";
 import type { AppProps } from "next/app";
+import Head from "@/src/components/items/Head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global styles={globalStyles} />
+      <Head />
       <Component {...pageProps} />
     </>
   );
