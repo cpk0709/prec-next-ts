d61b16fdbd95db53e335f60a7c2fbb015877da80
cpk0709 <cpk0709@gmail.com>
2023-04-10 22:14:27 +0900

    feat : add logger

---
+++ prec-next-ts/src/services/utility/logger.ts
@@ -0,0 +1,25 @@
+export const logger = {
+  debug: (...args: unknown[]): void => {
+    if (process.env.NODE_ENV === "development") {
+      console.debug(...args);
+    }
+  },
+  dir: (...args: unknown[]): void => {
+    if (process.env.NODE_ENV === "development") {
+      console.dir(...args);
+    }
+  },
+  info: (...args: unknown[]): void => {
+    if (process.env.NODE_ENV === "development") {
+      console.info(...args);
+    }
+  },
+  log: (...args: unknown[]): void => {
+    if (process.env.NODE_ENV === "development") {
+      console.log(...args);
+    }
+  },
+  error: (...args: unknown[]): void => {
+    console.error(...args);
+  },
+};
